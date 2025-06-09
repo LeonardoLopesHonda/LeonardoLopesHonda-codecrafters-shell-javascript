fcc9063b7c50de16a1e106f69cf71ac998849948
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-06-09 10:31:47 -0400

    feat: add `exit` functionality to shell

--- app/main.js
+++ app/main.js
@@ -7,9 +7,21 @@ const rl = readline.createInterface({
 
 function prompt() {
   rl.question("$ ", (answer) => {
-    console.log(`${answer}: command not found`);
-    prompt();
+    const command = answer.split(" ")[0];
+
+    switch (command) {
+      case "exit":
+        handleExit(answer);
+      default:
+        console.log(`${answer}: command not found`);
+        prompt();
+    }
   });
 }
 
 prompt();
+
+function handleExit(input) {
+  const exitCode = input.split(" ")[1];
+  process.exit(exitCode);
+}
